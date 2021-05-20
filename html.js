0b0239577493824a62f92fd5f467e44c6cfcec45
Your Name <you@example.com>
2021-05-20 15:29:57 +0300

    installed live-server, added server script

--- html.js
+++ html.js
@@ -2,3 +2,6 @@ import mainTitle from "./js/main_title.js";
 
 console.log(mainTitle);
 document.body.append(mainTitle("This is like a component"));
+
+// 9N susiintaliuoti live-server kaip dev priklausomybe, ir paleisti savo index.html faila
+// pasibandyti ar narsykleje duomenys atsinaujina pakeitus html ar css
